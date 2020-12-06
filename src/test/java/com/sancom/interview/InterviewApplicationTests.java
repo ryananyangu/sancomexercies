981a92983b75149119593e7132e39ea239f263fd
ryananyangu <ryananyangu@gmail.com>
2020-12-06 15:35:12 +0300

    initial setup

---
+++ src/test/java/com/sancom/interview/InterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sancom.interview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
